ba83da0a182b8d6f01e695764a495974b9271745
Goli Emarloo <goliemarloo1388@gmail.com>
2026-09-15 23:24:05 +0430

    Enable HSTS for production

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 SECURE_SSL_REDIRECT = not DEBUG
 SESSION_COOKIE_SECURE = not DEBUG
 CSRF_COOKIE_SECURE = not DEBUG
+SECURE_HSTS_SECONDS = 31536000
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
