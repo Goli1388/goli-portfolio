b912dc1177ab98add6cb5831d2a97e7e187f9ed2
Goli Emarloo <goliemarloo1388@gmail.com>
2026-09-17 11:34:44 +0430

    Improve secret key handling

--- config/settings.py
+++ config/settings.py
@@ -13,19 +13,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    'dev-only-secret-key-change-before-production'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise Exception("SECRET_KEY environment variable is not set")
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
